8c57639a0d77fb315c5d84aaa014cda2b9ee507b
alexfg <alexandre.franc@gmail.com>
2025-12-17 21:15:22 +0100

    route POST /posts v2 reference hashtags when they exist

--- routes/posts.js
+++ routes/posts.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const twitter = require('twitter-text');
+
+// X provides an Open Source twitter-text library that can be found on GitHub
+// https://github.com/twitter/twitter-text/blob/master/js/README.md
+
+// const usernames = twitter.extractMentions("Mentioning @twitter and @jack")
+// // usernames == ["twitter", "jack"]
+
+// const html = twitter.autoLink("link @user, please #request");
+// // html == link @<a class="tweet-url username" href="https://twitter.com/user" data-screen-name="user" rel="nofollow">user</a>, please <a href="https://twitter.com/search?q=%23request" title="#request" class="tweet-url hashtag" rel="nofollow">#request</a>
+
+// const hashTags = twitter.extractHashtags("link @user, please #request #WooT")
+// hashTags == [ 'request', 'WooT' ]
+
 const { checkBody, getUser } = require('../modules/common');
 const Post = require('../models/posts');
+const Hashtag = require('../models/hashtags');
 
 const POSTS_MAX_LENGTH = 280;
 
 router.post('/', async (req, res, next) => {
 
     const { token, content } = req.body;
+    const postHashtags = [];
 
     if (!checkBody(req.body, ['token', 'content'])) {
         res.json({ 
@@ -35,11 +51,20 @@ router.post('/', async (req, res, next) => {
         return;
     }
 
-//  hashtag processing required ...
+    for (const hashtag of twitter.extractHashtags(content)) { // can't use "await" in a forEach ...
+        const dbHashtag = await Hashtag.findOne({name: new RegExp(`^${hashtag}$`, 'i')});
+        if (dbHashtag) {
+            // le hastag existe, on pouse son id dans la liste des references du post
+            postHashtags.push(dbHashtag._id);
+        } else {
+            // il faut creer le hashtag avant de pousser l'id dans les ref du post
+        }
+    }
 
 	const newPost = await new Post({
         content: content,
 		userId: userDetails._id,
+        hashtags: postHashtags,
 	});
 	
 	await newPost.save();
