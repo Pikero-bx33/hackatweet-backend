44ea192ad88a06ad50737718355045b6571df245
alexfg <alexandre.franc@gmail.com>
2025-12-18 21:34:30 +0100

    init skeleton /users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const bcrypt = require("bcrypt");
+const uid2 = require("uid2");
+const { checkBody } = require('../modules/common');
+const Post = require('../models/users');
+
+router.post('/signup', (req, res) => {
+
+  const { username, fullName, password } = req.body;
+
+  if (!checkBody(req.body, ['username', 'fullName', 'password'])) {
+      res.json({
+          result: false,
+          error: 'Missing or empty fields.'
+      });
+      return;
+  }
+
+  // output ok: { result: true, username, fullName, token }
+  // output err: { result: false, error: "Username not available." }
+
+  res.send({result: true});
+
+});
+
+router.post('/signin', (req, res) => {
+
+  const { username, password } = req.body;
+
+    if (!checkBody(req.body, ['username', 'password'])) {
+      res.json({
+          result: false,
+          error: 'Missing or empty fields.'
+      });
+      return;
+  }
+
+  // output ok: { result: true, username, fullName, token }
+  // output err: { result: false, error: "Wrong username or password." }
+
+  res.send({result: true});
 });
 
 module.exports = router;
