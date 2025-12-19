92bc5ff26ef343b54c56d23b7e1d921387c68f08
alexfg <alexandre.franc@gmail.com>
2025-12-19 19:11:20 +0100

    POST /users/signup v1

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const bcrypt = require("bcrypt");
 const uid2 = require("uid2");
 const { checkBody } = require('../modules/common');
-const Post = require('../models/users');
+const User = require('../models/users');
 
 router.post('/signup', (req, res) => {
 
@@ -18,10 +18,34 @@ router.post('/signup', (req, res) => {
       return;
   }
 
-  // output ok: { result: true, username, fullName, token }
-  // output err: { result: false, error: "Username not available." }
+  // From morning-news-part5 + adjust:
+  // Check if the user has not already been registered
+  User.findOne({ username: new RegExp(`^${username}$`, 'i')}).then(data => {
+    if (data === null) {
 
-  res.send({result: true});
+      const newUser = new User({
+        username: username,
+        fullName: fullName,
+        password: bcrypt.hashSync(password, 10),
+        token: uid2(32),
+      });
+
+      newUser.save().then(newDoc => {
+        res.json({ 
+          result: true, 
+          userId: {
+              username: newDoc.username,
+              fullName: newDoc.fullName,
+              token: newDoc.token,
+            },
+          });
+      });
+    } else {
+      // User already exists in database
+      res.json({ result: false, error: 'User already exists.' });
+    }
+  });
+  // output ok: { result: true, username, fullName, token }
 
 });
 
